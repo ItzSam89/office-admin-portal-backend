ea2dfbfc9a2611078f2918782892037c308c6f58
Samriddhi <148458057+ItzSam89@users.noreply.github.com>
2026-03-18 11:07:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || '*',
+  origin: '*',
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   credentials: true
 }));
